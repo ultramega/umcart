f4c36daebc07475d0e48359f63d5f91e8ee36bde
Steve Guidetti <sguidetti@ultramegasoft.com>
2011-11-02 13:20:26 -0700

    Add default product image

--- web/app/command/viewproduct.class.php
+++ web/app/command/viewproduct.class.php
@@ -20,11 +20,17 @@ class Command_ViewProduct extends Command_Common {
             $product->loadCategories();
             $product->loadAttributes();
             
-            $this->data['product'] = $product->getAll();
-            $this->data['product']['title'] = String::safeHTMLText($this->data['product']['title']);
-            $this->data['product']['description'] = nl2br(String::safeHTMLText($this->data['product']['description']));
-            $this->data['product']['price'] = String::formatMoney($this->data['product']['price']);
-            $this->data['product']['image'] = String::safeHTMLText($this->data['product']['image']);
+            $product_data = $product->getAll();
+            $product_data['title'] = String::safeHTMLText($product_data['title']);
+            $product_data['description'] = nl2br(String::safeHTMLText($product_data['description']));
+            $product_data['price'] = String::formatMoney($product_data['price']);
+            
+            if(is_null($product_data['image'])) {
+                $product_data['image'] = 'default.png';
+            }
+            
+            $this->data['product'] = $product_data;
+            
             $this->loadCategories($product);
             $this->loadAttributes($product);
 
